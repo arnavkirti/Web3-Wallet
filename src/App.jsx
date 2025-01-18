48f4f8bcdc0d2dd59c45536d12c5fd44d3c87f8b
Arnav Kirti <arnav.tkd@gmail.com>
2025-01-19 01:28:53 +0530

    feat: made landing page and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { generateMnemonic } from "bip39";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mnemonic, setMnemonic] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-gray-900 text-white px-4">
+      <button
+        onClick={() => {
+          const mn = generateMnemonic();
+          setMnemonic(mn);
+        }}
+        className="mb-4 px-6 py-2 bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      >
+        Generate Seed Phrase
+      </button>
+      {mnemonic && (
+        <p className="text-center text-sm leading-6 p-4 border border-gray-700 rounded-md bg-gray-800 max-w-xl break-words">
+          {mnemonic}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
